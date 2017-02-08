6b95e6fd165b3cba58aa205e8da9b43ffac31d64
David Pratt <David Pratt>
2017-02-07 22:01:03 -0800

    Cleans up the code a little

--- dep-hero-image.php
+++ dep-hero-image.php
@@ -12,8 +12,8 @@
  * @package         Customizeable_Pages
  */
 
-
-function dep_hero_image_html($post) {
+function dep_hero_image_html($post)
+{
 
     $dir = plugin_dir_path( __FILE__ );
 
@@ -21,8 +21,7 @@ function dep_hero_image_html($post) {
 
 }
 
-
-function add_custom_box()
+function dep_hero_image_add_custom_box()
 {
     $screens = [ 'page' ];
     foreach ($screens as $screen) {
@@ -37,49 +36,47 @@ function add_custom_box()
     }
 }
 
-add_action('add_meta_boxes', 'add_custom_box');
-
+add_action('add_meta_boxes', 'dep_hero_image_add_custom_box');
 
 function dep_hero_image_save_postdata($post_id)
 {
 
-        // Bail if we're doing an auto save
-        if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+    // Bail if we're doing an auto save
+    if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
 
-        // if our nonce isn't there, or we can't verify it, bail
-       //  if( !isset( $_POST['meta_box_nonce'] ) || !wp_verify_nonce( $_POST['meta_box_nonce'], 'dep_hero_image_nonce' ) ) return;
+    // if our nonce isn't there, or we can't verify it, bail
+   //  if( !isset( $_POST['meta_box_nonce'] ) || !wp_verify_nonce( $_POST['meta_box_nonce'], 'dep_hero_image_nonce' ) ) return;
 
-        // if our current user can't edit this post, bail
-        if( !current_user_can( 'edit_post' ) ) return;
+    // if our current user can't edit this post, bail
+    if( !current_user_can( 'edit_post' ) ) return;
 
-        // now we can actually save the data
-        $allowed = array(
-            'a' => array( // on allow a tags
-                'href' => array() // and those anchors can only have href attribute
-            ),
-            'span' => array(
-                'style' => array(),
-                'class' => array()
-            ),
-            'br' => array(),
-            'em' => array(),
-            'strong' => array(),
-            'div' => array()
-        );
+    // now we can actually save the data
+    $allowed = array(
+        'a' => array( // on allow a tags
+            'href' => array() // and those anchors can only have href attribute
+        ),
+        'span' => array(
+            'style' => array(),
+            'class' => array()
+        ),
+        'br' => array(),
+        'em' => array(),
+        'strong' => array(),
+        'div' => array()
+    );
 
-        // Make sure your data is set before trying to save it
-        if( isset( $_POST['dep_hero_image_url'] ) ) {
+    // Make sure your data is set before trying to save it
+    if( isset( $_POST['dep_hero_image_url'] ) ) {
 
-            update_post_meta( $post_id, 'dep_hero_image_url', wp_kses( $_POST['dep_hero_image_url'], $allowed ) );
+        update_post_meta( $post_id, 'dep_hero_image_url', wp_kses( $_POST['dep_hero_image_url'], $allowed ) );
 
-        }
+    }
 
-        if( isset( $_POST['dep_hero_image_content'] ) ) {
+    if( isset( $_POST['dep_hero_image_content'] ) ) {
 
-            update_post_meta( $post_id, 'dep_hero_image_content', wp_kses( $_POST['dep_hero_image_content'], $allowed ) );
+        update_post_meta( $post_id, 'dep_hero_image_content', wp_kses( $_POST['dep_hero_image_content'], $allowed ) );
 
-        }
-
+    }
 
 }
 
@@ -89,7 +86,6 @@ function dep_hero_image_move_metaboxes()
 {
 
     // Move all "advanced" metaboxes above the default editor
-
     global $post, $wp_meta_boxes;
     do_meta_boxes(get_current_screen(), 'advanced', $post);
     unset($wp_meta_boxes[get_post_type($post)]['advanced']);
